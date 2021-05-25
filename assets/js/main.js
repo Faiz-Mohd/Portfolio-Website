5b2b1ccbd142d80447a419d74f23a395c6fef64a
Faiz <faizmohd.kdr@gmail.com>
2021-05-25 19:25:42 +0530

    Project filter added

--- assets/js/main.js
+++ assets/js/main.js
@@ -80,3 +80,21 @@ if ($('.nav-menu').length) {
 } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
   $(".mobile-nav, .mobile-nav-toggle").hide();
 }
+
+  // Porfolio isotope and filter
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+    });
+
+  });
