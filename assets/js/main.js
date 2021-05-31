c6dcbe4948c02dbf2666abbdc56073ecf6fdbb78
Faiz <faizmohd.kdr@gmail.com>
2021-05-31 15:48:01 +0530

    project page stylings

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,11 @@ if ($('.nav-menu').length) {
     });
 
   });
+
+    // Portfolio details carousel
+    $(".portfolio-details-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1
+    });
